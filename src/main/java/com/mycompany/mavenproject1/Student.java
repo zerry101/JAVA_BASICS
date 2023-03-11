3f0d4b6957bee3ba912fce773c2b84953c93481c
zerry101 <macone394@gmail.com>
2023-03-11 22:30:41 +0530

    java basics

---
+++ src/main/java/com/mycompany/mavenproject1/Student.java
@@ -0,0 +1,49 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mavenproject1;
+
+/**
+ *
+ * @author macon
+ */
+public class Student {
+
+    int studentId;
+    String studentName;
+    String studentCity;
+    
+    
+// Non parameterized constructor
+     public Student() {
+         System.out.println("non-parameterized constructor creating object....");    
+    }
+    
+//     parameterized constructor
+     public Student(int studentId,String studentName,String studentCity){
+         this.studentId=studentId;
+         this.studentName=studentName;
+         this.studentCity=studentCity;
+     }
+   
+    
+    
+//    Behaviour of class
+    public void study(){
+        System.out.println(studentName+ "is studying");
+    }
+//    Behaviour of class
+    public void studentDetails(){
+        System.out.println("My name is "+this.studentName);
+        System.out.println("My id is "+this.studentId);
+        System.out.println("My city is "+this.studentCity);
+        System.out.println();
+    }
+    
+    @Override
+    public String toString(){
+    return this.studentId+this.studentCity+this.studentName;
+    }
+    
+}
---
+++ src/main/java/com/mycompany/mavenproject1/demo.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.mavenproject1;
+
+/**
+ *
+ * @author macon
+ */
+public class demo {
+
+    public static void main(String[] args) {
+
+        Student st1;
+
+        st1 = new Student();
+
+        
+        st1=new Student(0,"zis","abad");
+        
+//        st1.studentCity = "ahmedabad";
+////        st1.studentid=123;
+//        st1.studentCity="delhi";
+        
+        st1.study();
+        st1.studentDetails();
+        System.out.println(st1.toString()); 
+    }
+
+}
